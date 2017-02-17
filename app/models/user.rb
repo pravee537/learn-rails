cc07dd1581af544f0f925bea78f4b573e9aa0627
praveen <praveenvjpm@gmail.com>
2017-02-17 16:55:32 +0530

    Finish learn-rails

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+	has_many :microposts
+	 validates content, presence: true    # Replace FILL_IN with the right code.
+  validates user, presence: true    # Replace FILL_IN with the right code.
 end
