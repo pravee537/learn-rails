6e1fddfc32781cd8d6c4f4e5aedbe8ec613d2d70
pravee537 <praveenvjpm@gmail.com>
2017-02-20 15:45:54 +0530

    Implement advanced login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220062454) do
+ActiveRecord::Schema.define(version: 20170220094140) do
 
   create_table "microposts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "content",    limit: 65535
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20170220062454) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
 end
