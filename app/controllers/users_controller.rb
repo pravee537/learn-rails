92c1f8b916a7a49acd8c9f6020d3c35449ecb161
pravee537 <praveenvjpm@gmail.com>
2017-02-20 15:00:11 +0530

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # Handle a successful save.
-       flash[:success] = "Welcome to the Sample App!"
+       flash[:success] = "Welcome to the LEMS"
        redirect_to @user
     else
       render 'new'
