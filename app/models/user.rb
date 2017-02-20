500c1c11b40dfd40fdd66dac54061ea84a89cf62
pravee537 <praveenvjpm@gmail.com>
2017-02-20 11:59:28 +0530

    Make a basic User model (including secure passwords)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ApplicationRecord
-	has_many :microposts
-  validates :content, presence: true    # Replace FILL_IN with the right code.
-  validates :user, presence: true    # Replace FILL_IN with the right code.
+	 before_save { email.downcase! }
+	#has_many :microposts
+ # validates :content, presence: true    # Replace FILL_IN with the right code.
+ # validates :user, presence: true    # Replace FILL_IN with the right code.
+   validates :name,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                      uniqueness: { case_sensitive: false }
+
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170217111105) do
+ActiveRecord::Schema.define(version: 20170220062454) do
 
   create_table "microposts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.text     "content",    limit: 65535
@@ -22,8 +22,9 @@ ActiveRecord::Schema.define(version: 20170217111105) do
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
