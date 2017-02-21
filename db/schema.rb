e241fa2bd2c51b5b29295f633268dc16f606ea31
pravee537 <praveenvjpm@gmail.com>
2017-02-21 10:50:54 +0530

    Finish user edit, update, index, and destroy actions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220094140) do
+ActiveRecord::Schema.define(version: 20170221045807) do
 
   create_table "microposts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "content",    limit: 65535
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20170220094140) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.boolean  "admin"
   end
 
 end
